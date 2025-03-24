170958a414093d72d99dfd02d16d3758bb4f43f5
meow75cheese <cassandra.burke1@icloud.com>
2025-03-24 17:00:42 +0000

    Pending changes exported from your codespace

--- lessons/00_Turtles/10c_Tash_Me_Twirl.py
+++ lessons/00_Turtles/10c_Tash_Me_Twirl.py
@@ -7,5 +7,55 @@ so the moustache will twirl when you click on it.
 Hint: See 08a_More Turtle Programs, section 'Click on the Turtle'
 """
 
-... # Your code here
+import turtle as turtle
+
+screen = turtle.Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('white')
+
+t = turtle.Turtle()
+
+def set_turtle_image(turtle, image_name):
+    """Set the turtle's shape to a custom image."""
+
+    from pathlib import Path
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    screen = turtle.getscreen()
+    screen.addshape(image_path)
+    turtle.shape(image_path)
+
+def set_background_image(window, image_name):
+    """Set the background image of the turtle window to the image with the given name."""
+
+    from pathlib import Path
+    from PIL import Image
+
+
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    image = Image.open(image_path)
+    
+    window.setup(image.width, image.height, startx=0, starty=0)
+    window.bgpic(image_path)
+
+set_turtle_image(t, "moustache1.gif")
+set_background_image(screen, "emoji.png") #🤔
+t.penup()
+
+t.left(90)
+t.forward(10)
+t.right(90)
+t.speed(3)
+
+def meoew(x,y):
+    print("miao")
+    t.speed(9)
+    t.forward(1000)
+
+screen.onclick(meoew)
+
+turtle.done()
 

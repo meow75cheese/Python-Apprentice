bc4850a3d5b97ef8c5dd3a47b8c66aa04b33022d
meow75cheese <cassandra.burke1@icloud.com>
2025-04-07 18:06:05 +0000

    what

--- lessons/00_Turtles/15_Pentagon_Crazy.py
+++ lessons/00_Turtles/15_Pentagon_Crazy.py
@@ -17,24 +17,25 @@ window = turtle.Screen()
 window.bgcolor("black")
 window.setup(width=600, height=600, startx=0, starty=0)
 
-colors = ("red", "blue", "green", "yellow", "orange")
+colors = ("red", "purple", "blue", "cyan", "turquoise", "green", "yellow", "orange")
 
 myTurtle = turtle.Turtle()
 myTurtle.shape("turtle")
 myTurtle.speed(0)
-myTurtle.width(1)
+myTurtle.width(2)
 
 sides = 5
 angle = 360 / sides
 
 for i in range(360):
     if i == 100:
-        myTurtle.width(2)
+        myTurtle.width(4)
     if i == 200:
-        myTurtle.width(3)
+        myTurtle.width(6)
     myTurtle.pencolor(getNextColor(i))
     myTurtle.forward(i)
-    myTurtle.right(angle + 1)
+    myTurtle.right(angle - 0.5)
+    myTurtle.left(20)
 
 myTurtle.hideturtle()
 
